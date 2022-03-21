aa64c471a6e703dd7d125c4579754dc9e2b93abc
lx-motto <101992389+lx-motto@users.noreply.github.com>
2022-03-21 20:18:10 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ class Spaceship{
      * @param {number} _firepower 
      * @param {number} _criticalstrikechance
      * @param {number} _armor
-     * homework : armor [done?]
-     * homework: make second ship fire back[done]
+     * homework : armor
+     * homework: make second ship fire back
      * homework : make github account with repo [done]
      */
     constructor(_name, _hp, _firepower,_criticalstrikechance,_armor){
@@ -25,27 +25,23 @@ class Spaceship{
      */
     fireAtTarget(target){
         console.log(this.name + " damaged " + target.name )
-        let damage = this.power;
+        let damage = this.power - target.armor;
         if( this.crit > Math.random()){
             damage = damage * 2;
             console.log("Critical HIT!!!!" )
         }
-        target.hp = (target.hp - damage)/(target.armor/100);
+        target.hp = target.hp - damage
         console.log(this.name + " has inflicted  " + damage + " damage" )
         console.log(target.name + " has  " + target.hp + " hp left" )
-
-        if(target.hp <= 0) {
-            console.log(target.name + " can no longer fight because it's HP is 0")
+        if(target.hp <= 0 || this.hp <=0) {
+            console.log(target.name + " was defeated by " + this.name)
         }
     }
 }
 
-let ship1 = new Spaceship("Voyager", 1000,100, 0.3, 100);
-let ship2 = new Spaceship("Enterprise", 800,50, 0, 100);
+let ship1 = new Spaceship("Voyager", 1000,100, 0.3, 20);
+let ship2 = new Spaceship("Enterprise", 800,50, 0, 20);
 
 while(ship2.hp > 0){
-    ship1.fireAtTarget(ship2)
-}
-while(ship1.hp > 0) {
-    ship2.fireAtTarget(ship1)
+    ship1.fireAtTarget(ship2) || ship2.fireAtTarget(ship1)
 }
